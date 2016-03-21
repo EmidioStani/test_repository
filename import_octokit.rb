ba196555be6dfb9f0346da161c79ff19ace6c9eb
Jens Scheerlinck <jens@jesc.be>
2016-03-21 10:46:37 +0100

    Added source files

---
+++ import_octokit.rb
@@ -0,0 +1,61 @@
+#encoding:UTF-8
+#!/usr/bin/env ruby
+require 'OctoKit'
+require 'CSV'
+require 'highline/import'
+
+
+
+# because github didn't display my random colors
+ISSUE_COLORS = ['d4c5f9','e11d21','eb6420','fbca04','009800','006b75','207de5',
+				'0052cc','5319e7','f7c6c7','fad8c7','fef2c0','bfe5bf','bfdadc',
+				'c7def8', 'bfd4f2']
+
+def get_input(prompt="Enter >",show = true)
+	ask(prompt) {|q| q.echo = show}
+end
+
+issues_csv = ARGV.shift or raise "Enter Filepath to CSV as ARG1"
+
+user = get_input('Enter Username >')
+password = get_input('Enter Password >', '*')
+
+client = Octokit::Client.new \
+	:login => user,
+	:password => password
+
+user = client.user
+user.login
+
+repo = get_input('Enter repo (owner/repo_name) >')
+
+Issue = Struct.new(:title, :body, :labels)
+issues = Array.new
+
+CSV.foreach issues_csv, headers: true, :row_sep => "\r\n", :col_sep => ";" do |row|
+	labels = []
+	labels << row['Labels'].split(',') unless row['Labels'].nil?
+	issues << Issue.new(row['Title'], row['Description'], labels)
+end
+
+unique_labels = issues.map{ |i| i.labels }.flatten.map{|j| j.to_s.strip}.uniq
+puts "adding labels: #{unique_labels.to_s}"
+unique_labels.each do |l|
+	begin
+		#client.add_label(repo, l, ISSUE_COLORS.sample)
+	rescue Octokit::UnprocessableEntity => e
+		puts "Unable to add #{l} as a label. Reason: #{e.errors.first[:code]}"
+	end
+end
+
+#start_time = Time.now
+
+issues.each_with_index do |issue, i|
+	puts "creating issue '#{issue.title}'"
+	issue_number = client.create_issue(repo, issue.title, issue.body, {:labels => issue.labels.join(',')}).number
+    #elapsed_seconds = ((Time.now - start_time) * 1000).to_i
+    #puts "'#{i}' - elapsed seconds: '#{elapsed_seconds}'"
+    if (i+1) % 30 == 0
+      sleep(10)
+    end
+end
--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
-this is a test repository
+# PivotalTracker to Github Issues migration scripts
+
+Rewrote the original import_issues.rb into import_octokit.rb to use github's official ruby gem. The original script wasn't working for me.
+
+To succesfully migrate tickets from PivotalTracker to Github do:
+
+1. Clone this repo
+2. Install the gems octokit and highline
+3. Import all user stories from PivotalTracker to csv file
+4. For importing tickets from csv to repo, run: `ruby import_octokit.rb your-issues.csv` and follow prompts
+
+I haven't looked at the delete_labels script yet, it could probably use some work.
+
+Original thanks for making these scripts to @robotarmy and his gist: https://gist.github.com/2257596
+
+Done!
+
+https://curl.haxx.se/ca/cacert.pem
+SET SSL_CERT_FILE=cacert.pem
